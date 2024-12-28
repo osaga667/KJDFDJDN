1367edcd29b45a669d62f7e49a6da45a4c2d331c
osaga667 <ihsanatasoy50@gmail.com>
2024-12-28 16:41:25 +0300

    Add files via upload

---
+++ assets/web.735e8ebb.js
@@ -0,0 +1,16 @@
+import { W as WebPlugin } from "./index.d7d9fc52.js";
+class ToastWeb extends WebPlugin {
+  async show(options) {
+    if (typeof document !== "undefined") {
+      let duration = 2e3;
+      if (options.duration) {
+        duration = options.duration === "long" ? 3500 : 2e3;
+      }
+      const toast = document.createElement("pwa-toast");
+      toast.duration = duration;
+      toast.message = options.text;
+      document.body.appendChild(toast);
+    }
+  }
+}
+export { ToastWeb };
